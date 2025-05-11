5c5a6d3021d8827dcc336b655f54b988ffae5c04
Suraj Kumar <skr090850@gmail.com>
2025-05-11 15:45:44 +0530

    Practice phase

---
+++ Suraj/my-app/src/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+    return(
+        <div className="footer">
+            <h1>Footer Section</h1>
+        </div>
+    )
+}
+export {Footer}
---
+++ Suraj/my-app/src/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return(
+    <div>
+      <h1>Header section</h1>
+    </div>
+  )
+}
+
+export {Header};
